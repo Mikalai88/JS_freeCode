044edeb965c02adc49bb943f1e3ea6fe670e5d30
Mikalai <mikalai.mikhalachkin@gmail.com>
2022-11-02 19:22:20 +0100

    2.11.3

--- codeWars.js
+++ codeWars.js
@@ -5,3 +5,7 @@ function even_or_odd(number) {
 function multiply(a, b){
     return a * b;
 }
+
+function createPhoneNumber(numbers){
+    return "(" + numbers[0] + numbers[1] + numbers[2] + ") " + numbers[3] + numbers[4] + numbers[5] + "-" + numbers[6] + numbers[7] + numbers[8] + numbers[9]
+}
