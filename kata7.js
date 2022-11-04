65b1489c56e9956606bd05060beeab5b6aae06f3
Mikalai <mikalai.mikhalachkin@gmail.com>
2022-11-04 18:26:55 +0100

    squareDigits

--- kata7.js
+++ kata7.js
@@ -1,3 +1,8 @@
 function disemvowel(str) {
     return str.replace(/[aeiou]/gi, '');
 }
+
+function squareDigits(num){
+    return Number(('' + num).split('').map(function (val) { return val * val;}).join(''));
+
+}
