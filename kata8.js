07fa30c6b88a540710521bde8335fd447e66a84c
Mikalai <mikalai.mikhalachkin@gmail.com>
2022-11-04 19:26:02 +0100

    divisibleBy

--- kata8.js
+++ kata8.js
@@ -4,3 +4,11 @@ function aName(n){
     return first[0] + '.' + last[0];
 
 }
+
+function isDivisible(n, x, y) {
+    if (n % x == 0 && n % y == 0) {
+        return true;
+    } else {
+        return false;
+    }
+}
